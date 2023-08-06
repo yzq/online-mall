d552d1420b12a0fa56ea2825f160623b2bee01c2
yzq <yzq0610@gmail.com>
2023-08-06 22:34:30 +0800

    创建工程

---
+++ mall-mbg/src/main/java/com/macro/mall/MallMbgApplication.java
@@ -0,0 +1,13 @@
+package com.macro.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallMbgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MallMbgApplication.class, args);
+	}
+
+}
